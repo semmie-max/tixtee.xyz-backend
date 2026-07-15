f1c40a7128b7dff3154a914d68c25492e2920892
semmie-max <semmie162l@gmail.com>
2026-07-15 21:16:41 +0100

    edited authjs

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ function issueToken(res, user) {
   );
   res.cookie('token', token, {
     httpOnly: true,
-    sameSite: 'none',
+    sameSite: 'lax',
     secure: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
